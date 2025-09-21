2d1016368009b9b040f66c3d6ea23f3ec733dfbd
rimpa kasundi <rimpakasundi@gmail.com>
2025-09-22 01:15:10 +0530

    Add visited state toggle with dynamic styling for country component

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Country.css";
 
 const Country = ({ country }) => {
-  console.log(country.area.area);
+  const [visited, setVisited] = useState(false);
+  // console.log(country.area.area);
 
   const handleVisited = () => {
-    console.log("btn clicked");
+    // basic method
+    // if (visited) {
+    //   setVisited(false);
+    // } else {
+    //   setVisited(true);
+    // }
+    // second method
+    setVisited(visited ? false : true);
+
+    // third method
+    // setVisited(!visited)
   };
 
   return (
-    <div className="country">
+    <div
+      // that's better
+      className={`country border-lg text-center ${
+        visited ? "country-visited" : "country not visited"
+      }  `}
+
+      //  className={`country ${visited && "Country-visited"} `}
+    >
       <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
       <h3>Name: {country.name.common} </h3>
       <p>Population: {country.population.population}</p>
@@ -17,7 +35,9 @@ const Country = ({ country }) => {
         Area:{country.area.area}{" "}
         {country.area.area > 300000 ? "Big Country" : "Small Country"}
       </p>
-      <button onClick={handleVisited}>Not Visited</button>
+      <button onClick={handleVisited}>
+        {visited ? "Visited" : "Not Visited"}
+      </button>
     </div>
   );
 };
