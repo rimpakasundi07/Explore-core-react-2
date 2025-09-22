c225c118b4d004c1acd86cd05bdc90168c35ec2c
rimpa kasundi <rimpakasundi@gmail.com>
2025-09-22 18:12:19 +0530

    Implement visited countries tracking with React Suspense integration

--- src/components/countries/Countries.jsx
+++ src/components/countries/Countries.jsx
@@ -7,6 +7,8 @@ const Countries = ({ countriesPromise }) => {
 
   const handleVisitedCountries = (country) => {
     console.log("handle visited country clicked", country);
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
   };
 
   const countriesData = use(countriesPromise);
@@ -15,7 +17,7 @@ const Countries = ({ countriesPromise }) => {
   return (
     <div>
       <h1>In the Countries: {countries.length} </h1>
-      <h3>Total Country Visited : </h3>
+      <h3>Total Country Visited : {visitedCountries.length} </h3>
       <div className="countries">
         {countries.map((country) => (
           <Country
